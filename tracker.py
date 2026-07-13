0dffd1f755c7a467330e6a91f81323a93d0e737b
Haider Ali <haiderarai@gmail.com>
2026-07-13 17:41:22 +0500

    first Python project tacker.py code addition, added currency format function

--- tracker.py
+++ tracker.py
@@ -1,7 +1,8 @@
 def main():
-    user_name = input("Enter Your Name: ").strip().title()
-    bank_balance = int(input("Enter Your Bank Balance: "))
-    print(f"Welcome {user_name}, your bank balance is: {bank_balance}")
+    #user_name = input("Enter Your Name: ").strip().title()
+   # bank_balance = int(input("Enter Your Bank Balance: "))
+    #print(f"Welcome {user_name}, your bank balance is: {bank_balance}")
+    print(format_currency(2000.6363))
 
 def deposit(balance, amount):
     balance += amount
@@ -12,5 +13,15 @@ def deposit(balance, amount):
 def withdraw(balance, amount):
     balance -= amount
     return balance
+
+def format_currency(amount):
+    return f"${amount:,.2f}"
+    
+
+
+
+
+
+
 
 main()
