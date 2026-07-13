dfbc6a4ec5500e2d6c87a25450841e9c1518220c
Haider Ali <haiderarai@gmail.com>
2026-07-13 18:04:59 +0500

    first Python project tacker.py code addition for clinet balance withdraw process

--- tracker.py
+++ tracker.py
@@ -1,27 +1,22 @@
 def main():
-    #user_name = input("Enter Your Name: ").strip().title()
-   # bank_balance = int(input("Enter Your Bank Balance: "))
-    #print(f"Welcome {user_name}, your bank balance is: {bank_balance}")
-    print(format_currency(2000.6363))
+    user_name = input("Enter Your Name: ").strip().title()
+    bank_balance = int(input("Enter Your Bank Balance: "))
+    print(f"Welcome {user_name}")
+
+    action = input("Do you want to deposit or withdraw? ").strip().upper()
+    if action == "WITHDRAW":
+        withdrawal = float(input("How much do you want to withdraw: "))
+        print("Withdraw successful your ramining balance is:", withdraw(bank_balance, withdrawal))
 
 def deposit(balance, amount):
     balance += amount
     return balance
-
-
 
 def withdraw(balance, amount):
     balance -= amount
     return balance
 
 def format_currency(amount):
-    return f"${amount:,.2f}"
-    
-
-
-
-
-
-
+    return f"${amount:,.2f}"    
 
 main()
