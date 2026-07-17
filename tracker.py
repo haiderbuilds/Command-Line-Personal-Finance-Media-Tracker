9ce69e470c37146afdfe950a9a2e002cf10141aa
Haider Ali <haiderarai@gmail.com>
2026-07-17 17:23:31 +0500

    fixed some code style issues

--- tracker.py
+++ tracker.py
@@ -3,10 +3,10 @@ def main():
     bank_balance = int(input("Enter Your initial Bank Balance: "))
     print(f"Welcome {user_name}, your initial bank balance is: {bank_balance}")
 
-    action = input("Do you want to deposit or withdraw? ").strip().upper()
-    if action == "WITHDRAW":
+    action = input("Do you want to deposit or withdraw? ").strip().lower()
+    if action == "withdraw":
         withdrawal = float(input("How much do you want to withdraw: ")) 
-        category = input("What is the purpose of this withdraw? ").lower()
+        category = input("What is the purpose of this withdraw? ").strip().lower()
 
         if withdrawal > bank_balance:
             print("Insufficient funds.")
@@ -19,14 +19,14 @@ def main():
                 case "entertainment":
                     necessity_level = "Low"
                 case "clothes":
-                    necessity_level = "Meidum"
+                    necessity_level = "Medium"
                 case _:
                     print("Not a valid cetagory!")
             print("Necessity level:", necessity_level)
 
             bank_balance = withdraw(bank_balance, withdrawal)
             print("Withdraw Successful! Your remaining bank balance is:", bank_balance)
-    elif action == "DEPOSIT":
+    elif action == "deposit":
         deposit_amount = float(input("How much do you want to deposit: "))
         if deposit_amount > 0:
             bank_balance = deposit(bank_balance, deposit_amount)
