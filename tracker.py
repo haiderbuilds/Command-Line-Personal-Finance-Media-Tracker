0ec485a4d18a09099fb20998985650efe105872f
Haider Ali <haiderarai@gmail.com>
2026-07-18 17:55:31 +0500

    added the logic to make infinite ledger

--- tracker.py
+++ tracker.py
@@ -1,41 +1,43 @@
 def main():
     user_name = input("Enter Your Name: ").strip().title()
     bank_balance = int(input("Enter Your initial Bank Balance: "))
-    print(f"Welcome {user_name}, your initial bank balance is: {bank_balance}")
+    print(f"Welcome {user_name}, Your initial bank balance is: {bank_balance}")
+    while True:
+        action = input("Do you want to deposit or withdraw? ").strip().lower()
+        if action == "withdraw":
+            withdrawal = float(input("How much do you want to withdraw: ")) 
+            category = input("What is the purpose of this withdraw? ").strip().lower()
 
-    action = input("Do you want to deposit or withdraw? ").strip().lower()
-    if action == "withdraw":
-        withdrawal = float(input("How much do you want to withdraw: ")) 
-        category = input("What is the purpose of this withdraw? ").strip().lower()
+            if withdrawal > bank_balance:
+                print("Insufficient funds.")
+            else:
+                match category:
+                    case "rent":
+                        necessity_level = "High"
+                    case "food":
+                        necessity_level = "High"
+                    case "entertainment":
+                        necessity_level = "Low"
+                    case "clothes":
+                        necessity_level = "Medium"
+                    case _:
+                        print("Not a valid cetagory!")
+                print("Necessity level:", necessity_level)
 
-        if withdrawal > bank_balance:
-            print("Insufficient funds.")
+                bank_balance = withdraw(bank_balance, withdrawal)
+                print("Withdraw Successful! Your remaining bank balance is:", bank_balance)
+        elif action == "deposit":
+            deposit_amount = float(input("How much do you want to deposit: "))
+            if deposit_amount > 0:
+                bank_balance = deposit(bank_balance, deposit_amount)
+                print("Deposit Successful! Your bank balance is:",bank_balance)
+            else:
+                print("Enter correct amount.")
+        elif action == "quit":
+            break
         else:
-            match category:
-                case "rent":
-                    necessity_level = "High"
-                case "food":
-                    necessity_level = "High"
-                case "entertainment":
-                    necessity_level = "Low"
-                case "clothes":
-                    necessity_level = "Medium"
-                case _:
-                    print("Not a valid cetagory!")
-            print("Necessity level:", necessity_level)
-
-            bank_balance = withdraw(bank_balance, withdrawal)
-            print("Withdraw Successful! Your remaining bank balance is:", bank_balance)
-    elif action == "deposit":
-        deposit_amount = float(input("How much do you want to deposit: "))
-        if deposit_amount > 0:
-            bank_balance = deposit(bank_balance, deposit_amount)
-            print("Deposit Successful! Your bank balance is:",bank_balance)
-        else:
-            print("Enter correct amount.")
-    else:
-        print("Invalid Action.")
-    print(bank_balance)
+            print("Invalid Action, Try Again.")
+        print(bank_balance)
 
 def deposit(balance, amount):
     balance += amount
